8cf99edff1d4b2322dd2291d1adb930108983cac
bgungor62 <barangungor33@gmail.com>
2024-02-10 15:51:35 +0300

    Yazılan methodlar Api Consume edildi

--- HotelApiProject/ApiConsume/HotelWebApi/Controllers/DashboardWidgetController.cs
+++ HotelApiProject/ApiConsume/HotelWebApi/Controllers/DashboardWidgetController.cs
@@ -9,16 +9,43 @@ namespace HotelWebApi.Controllers
     public class DashboardWidgetController : ControllerBase
     {
         private readonly IStaffService _staffService;
+        private readonly IBookingService _bookingService;
+        private readonly IGuestService _guestService;
+        private readonly IRoomService _roomService;
 
-        public DashboardWidgetController(IStaffService staffService)
+        public DashboardWidgetController(IStaffService staffService, IBookingService bookingService, IGuestService guestService,IRoomService roomService)
         {
             _staffService = staffService;
+            _bookingService = bookingService;
+            _guestService = guestService;
+            _roomService = roomService;
         }
 
         [HttpGet("staffCount")]
-        public IActionResult StaffCount()
+        public IActionResult GetCountStaff()
         {
             var values = _staffService.TGetStaffCount();
+            return Ok(values);
+        }
+
+        [HttpGet("bookingCount")]
+        public IActionResult GetCountBooking()
+        {
+            var values = _bookingService.TGetCountBooking();
+            return Ok(values);
+        }
+
+        [HttpGet("guestCount")]
+        public IActionResult GetCountGuest()
+        {
+            var values = _guestService.TGetCountGuest();
+            return Ok(values);
+        }
+
+        [HttpGet("roomCount")]
+        public IActionResult GetCountRoom()
+        {
+            var values = _roomService.TGetCountRoom();
             return Ok(values);
         }
 
