e5bee292a8c3d9cb45a7a956a6b86e0c7c679f67
bgungor62 <barangungor33@gmail.com>
2024-02-10 15:51:52 +0300

    Update _DashboardWidgetPartial.cs

--- HotelApiProject/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardWidgetPartial.cs
+++ HotelApiProject/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardWidgetPartial.cs
@@ -2,10 +2,28 @@
 
 namespace HotelProject.WebUI.ViewComponents.Dashboard
 {
-    public class _DashboardWidgetPartial:ViewComponent
+    public class _DashboardWidgetPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardWidgetPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var staffCount = await client.GetFromJsonAsync<int>("http://localhost:5062/api/DashboardWidget/staffCount");
+            var bookingCount = await client.GetFromJsonAsync<int>("http://localhost:5062/api/DashboardWidget/bookingCount");
+            var guestCount = await client.GetFromJsonAsync<int>("http://localhost:5062/api/DashboardWidget/guestCount");
+            var roomCount = await client.GetFromJsonAsync<int>("http://localhost:5062/api/DashboardWidget/roomCount");
+
+
+            ViewBag.staffcount = staffCount;
+            ViewBag.bookingcount = bookingCount;
+            ViewBag.guestcount = guestCount;
+            ViewBag.roomcount = roomCount;
             return View();
         }
     }
