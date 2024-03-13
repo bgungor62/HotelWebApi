e686227e08f167795f88e4ce1cc0968a2a3c91ab
Baran Gungor <barangungor33@gmail.com>
2024-03-13 22:52:01 +0300

    Viewcomponents de  api consume edildi

---
+++ HotelApiProject/HotelProject.WebUI/ViewComponents/Dashboard/_DashboardLast5Bookings.cs
@@ -0,0 +1,29 @@
+﻿using HotelProject.WebUI.Dtos.BookingDto;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace HotelProject.WebUI.ViewComponents.Dashboard
+{
+    public class _DashboardLast5Bookings : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _DashboardLast5Bookings(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:5062/api/Booking/getLast5Bookings");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultBookingDto>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
